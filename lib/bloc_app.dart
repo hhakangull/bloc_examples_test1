bdb8540b767edd689a58dbcc4a396b46e50c8266
Hakan GÜL <hakanngul@icloud.com>
2022-09-21 01:35:00 +0300

    step-1

---
+++ lib/bloc_app.dart
@@ -0,0 +1,71 @@
+import 'dart:math' as math show Random;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+const names = ["Foo", "Bar", "Baz"];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() => emit(names.getRandomElement());
+}
+
+class HomePageView extends StatefulWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  State<HomePageView> createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  late final NamesCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Bloc App Home Page View"),
+      ),
+      body: StreamBuilder<String?>(
+          stream: cubit.stream,
+          builder: (context, snapshot) {
+            final button = TextButton(
+              onPressed: cubit.pickRandomName,
+              child: const Text('Pick a random name'),
+            );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return button;
+              case ConnectionState.waiting:
+                return button;
+              case ConnectionState.active:
+                return Column(
+                  children: [
+                    Text(snapshot.data ?? ''),
+                    button,
+                  ],
+                );
+              case ConnectionState.done:
+                return const SizedBox();
+            }
+          }),
+    );
+  }
+}
